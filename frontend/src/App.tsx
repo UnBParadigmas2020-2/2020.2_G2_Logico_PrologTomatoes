ffdbc5efc3461a057b9a7c7f83893020184d26e0
Daniel Sousa <daniel_sousa.unb@outlook.com>
2021-03-29 16:43:29 -0300

    Making frontend repository and first page

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import PrologTomatoesOne from './pages/PrologTomatoesOne';
+import GlobalStyle from './styles/global';
+
+const App: React.FC = () => (
+  <>
+    <PrologTomatoesOne />
+    <GlobalStyle />
+  </>
+);
+
+export default App;
